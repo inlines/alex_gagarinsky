349881a310c7e56154506c3501df65e236cd70b5
kirill zakirov <kir@MacBook-Pro-kirill.local>
2021-12-02 11:48:06 +0300

    elevator

--- api/add-score.php
+++ api/add-score.php
@@ -8,7 +8,7 @@ try {
   $score = $_POST['score'];
 
   $connection = mysqli_connect("ale1480792.mysql","ale1480792_mysql","OeeP2:OO","ale1480792_top_scrore") or die("Error " . mysqli_error($connection));
-  $sql = "INSERT INTO scores(is_alex, score, `name`) VALUES (0, ".$score.", '".$name_input."')";
+  $sql = "INSERT INTO scores(is_alex, score, `name`, `datetime`) VALUES (0, ".$score.", '".$name_input."', CURRENT_TIMESTAMP())";
   $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
 
   echo json_encode(0);
